32894ee013ebbbf52bd7576e8fd50f6ee61974d5
Hambinn <ilhambintangn@gmail.com>
2022-04-27 11:53:10 +0700

    fix LCS

--- src/Backend/LCS.go
+++ src/Backend/LCS.go
@@ -1,12 +1,21 @@
 package main
 
 func lcs(virus, human string, virLen, humLen int) int {
-	if virLen == 0 || humLen == 0 {
-		return 0
+	var T = make([][]int, virLen+1)
+	for i := range T {
+		T[i] = make([]int, humLen+1)
 	}
-	if virus[virLen-1] == human[humLen-1] {
-		return 1 + lcs(virus, human, virLen-1, humLen-1)
-	} else {
-		return max(lcs(virus, human, virLen-1, humLen), lcs(virus, human, virLen, humLen-1))
+
+	for i := 0; i < virLen+1; i++ {
+		for j := 0; j < humLen+1; j++ {
+			if i == 0 || j == 0 {
+				T[i][j] = 0
+			} else if virus[i-1] == human[j-1] {
+				T[i][j] = 1 + T[i-1][j-1]
+			} else {
+				T[i][j] = max(T[i-1][j], T[i][j-1])
+			}
+		}
 	}
+	return T[virLen][humLen]
 }
